a0163441fb58332212758abb31cf3bbf85a9b00b
syedali <jangomango4u@gmail.com>
2025-12-31 23:06:50 +0500

    Production Build: Digital Riyal Treasury

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {},
-    global: 'globalThis',
-  },
-  build: {
-    target: 'esnext'
-  }
-});
+export default defineConfig({ plugins: [react()], define: { global: 'globalThis' } });
