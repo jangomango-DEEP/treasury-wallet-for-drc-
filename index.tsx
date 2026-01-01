321c1d2ea18b1145a7ad6bb69d5419db1ab215cc
jangomango-DEEP <jangomango@hotmail.com>
2026-01-01 23:45:11 +0500

    Production Build: Digital Riyal Treasury

--- index.tsx
+++ index.tsx
@@ -2,19 +2,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-// Ensure Buffer is globally available for web3 libraries
-if (typeof window !== 'undefined' && !(window as any).Buffer) {
-  // If the script tag in index.html didn't load yet or failed, this is a fallback.
-  // Note: Most web3 modules check global Buffer on load.
-}
-
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Root element not found");
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<React.StrictMode><App /></React.StrictMode>);
